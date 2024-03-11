28efe3a4539e9e794bb90b497c009596645c95c0
sarangsalunkhe <sarangsalunkhe2@gmail.com>
2024-03-11 17:51:07 +0530

    Add project files.

---
+++ Test_Student_Application/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Test_Student_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStudnt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtstudentaddress = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtStudnt
+            // 
+            this.txtStudnt.Location = new System.Drawing.Point(88, 59);
+            this.txtStudnt.Name = "txtStudnt";
+            this.txtStudnt.Size = new System.Drawing.Size(235, 20);
+            this.txtStudnt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student Name";
+            // 
+            // txtstudentaddress
+            // 
+            this.txtstudentaddress.Location = new System.Drawing.Point(362, 158);
+            this.txtstudentaddress.Name = "txtstudentaddress";
+            this.txtstudentaddress.Size = new System.Drawing.Size(235, 20);
+            this.txtstudentaddress.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(147, 214);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(327, 176);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 643);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtstudentaddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStudnt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStudnt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtstudentaddress;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Test_Student_Application/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Test_Student_Application
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog fd1 = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+        public System.Drawing.Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
+            return returnImage;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog obj = new OpenFileDialog();
+            
+                obj.FilterIndex = 1;
+                DialogResult file = obj.ShowDialog();
+                {
+                    pictureBox1.Image = System.Drawing.Image.FromFile(obj.FileName);
+                   // this.l,  = imageToByteArray(System.Drawing.Image.FromFile(obj.FileName));
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
